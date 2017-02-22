b859ed443467e7c316b1d0c0d2ee846b7d173077
Adam Reisinger <reisinger.adam@gmail.com>
2017-02-22 11:57:13 +0100

    Add more browsers to karma.

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,23 @@
 module.exports = function(config) {
   config.set({
     frameworks: ["jasmine"],
-    browsers: ["Chrome"],
+    browsers: ["Chrome", "Firefox", "Opera", "Edge", "IE", "IE10", "IE9", "IE8"],
     //noResolve: false,
+
+    customLaunchers: {
+      IE10: {
+        base: 'IE',
+        'x-ua-compatible': 'IE=EmulateIE10'
+      },
+      IE9: {
+        base: 'IE',
+        'x-ua-compatible': 'IE=EmulateIE9'
+      },
+      IE8: {
+        base: 'IE',
+        'x-ua-compatible': 'IE=EmulateIE8'
+      }
+    },
 
     files: [
         "date-slider.js",
