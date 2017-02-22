9ca23b6c5fed8a6f10f50696969f974ce4e938be
Adam Reisinger <reisinger.adam@gmail.com>
2017-02-22 15:36:46 +0100

    Add default options.

--- src/date-slider.ts
+++ src/date-slider.ts
@@ -1,5 +1,11 @@
 module DateSlider {
+    export let defaults: DateSliderOptions = {};
+
     export function create(element: HTMLElement, options: DateSliderOptions): DateSliderInstance {
-        return new DateSliderInstance(element, options);
+        return new DateSliderInstance(element, mergeOptions(options, defaults));
+    }
+
+    function mergeOptions(instanceOptions: DateSliderOptions, defaultOptions: DateSliderOptions): DateSliderOptions {
+        return instanceOptions;
     }
 }
