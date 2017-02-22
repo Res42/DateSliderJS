4dceaf6931b3bf88d15b45b27d1a9e0148a73bd9
Adam Reisinger <reisinger.adam@gmail.com>
2017-02-23 00:22:00 +0100

    Add less files. Configure gulp to compile them.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,30 @@ var sourcemaps = require("gulp-sourcemaps");
 var merge = require('merge2');
 var appTsProject = ts.createProject("./tsconfig.json");
 var testTsProject = ts.createProject("./test/tsconfig.json");
+var less = require("gulp-less");
+var cleanCss = require("gulp-clean-css");
+var rename = require("gulp-rename");
 
 gulp.task("app", function () {
     var app = appTsProject.src()
         .pipe(sourcemaps.init())
         .pipe(appTsProject());
 
+    
+
     return merge([
         app.dts.pipe(gulp.dest(".")),
         app.js.pipe(sourcemaps.write(".")).pipe(gulp.dest("."))
+    ]);
+});
+
+gulp.task("css", function() {
+    var compileLess = gulp.src("src/date-slider.less")
+        .pipe(less());
+
+    return merge([
+        compileLess.pipe(gulp.dest(".")),
+        compileLess.pipe(cleanCss({compatibility: "ie8"})).pipe(rename({extname: ".min.css"})).pipe(gulp.dest("."))
     ]);
 });
 
@@ -24,9 +39,10 @@ gulp.task("test", ["app"], function () {
         .pipe(gulp.dest("./test"));
 });
 
-gulp.task("build", ["app", "test"]);
+gulp.task("build", ["app", "test", "css"]);
 
 gulp.task("default", ["build"], function() {
     gulp.watch("src/**/*.ts", ["app"]);
     gulp.watch("test/**/*.ts", ["test"]);
+    gulp.watch("src/**/*.less", ["css"]);
 });
