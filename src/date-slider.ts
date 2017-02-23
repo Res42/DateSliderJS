a07b6d4fa4b47f17b02bff0768040ef0350276d7
Szabó Gábor (gabe) <yugabe@gmail.com>
2017-02-23 11:15:36 +0100

    Consultation results

--- src/date-slider.ts
+++ src/date-slider.ts
@@ -10,16 +10,18 @@ module DateSlider {
     }
 
     // TODO
-    // should we store the raw value?
-    // return raw value if it is invalid?
-    // date parse/format: utc, local?
-    // use abstractformatter or not? writing own formatter may be a hassle if used
+    // should we store the raw value? -> yes
+    // return raw value if it is invalid? -> throw
+    // date parse/format: utc, local? -> timezone to model
+    // use abstractformatter or not? writing own formatter may be a hassle if used -> CustomFormatter/Parser should take the callback as constructor param
 
     // file: 'unix-timestamp-ms-formatter.ts'
     // severity: 'Error'
     // message: 'A class must be declared after its base class.'
     // at: '2,51'
-    // source: 'ts'
+    // source: 'ts' -> ///<reference ...
 
-    //  Date.parse() or write own implementation to parse from formats
+    //  Date.parse() or write own implementation to parse from formats -> own
+
+    // next week -> create demo page, create DOM elements, hooks
 }
